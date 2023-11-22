e1b4a581a6b71154cc8c9897c0749931b6ab6ac5
Giogio-armani <giovan.hamadaine@hotmail.com>
2023-11-22 10:44:07 +0100

    gestion error front

--- client/src/components/log/SignUpForm.js
+++ client/src/components/log/SignUpForm.js
@@ -1,6 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import SignInForm from "./SignInForm";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  pseudo: Yup.string()
+    .required("Le pseudo est requis")
+    .min(3, "Trop court")
+    .max(55, "Trop long"),
+  email: Yup.string().email("Email invalide").required("L'email est requis"),
+  password: Yup.string()
+    .required("Le mot de passe est requis")
+    .min(6, "Trop court")
+    .max(1024, "Trop long"),
+  controlPassword: Yup.string()
+    .required("La confirmation est requise")
+    .oneOf(
+      [Yup.ref("password"), null],
+      "Les mots de passe doivent correspondre"
+    ),
+  terms: Yup.boolean().oneOf(
+    [true],
+    "Veuillez accepter les conditions générales"
+  ),
+});
 
 const SignUpForm = () => {
   const [pseudo, setPseudo] = useState("");
@@ -11,48 +34,88 @@ const SignUpForm = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const terms = document.getElementById("terms");
-    const pseudoError = document.querySelector(".pseudo.error");
-    const emailError = document.querySelector(".email.error");
-    const passwordError = document.querySelector(".password.error");
-    const passwordConfirmError = document.querySelector(
-      ".password-confirm.error"
-    );
-    const termsError = document.querySelector(".terms.error");
 
-    passwordConfirmError.innerHTML = "";
-    termsError.innerHTML = "";
+    try {
+      const terms = document.getElementById("terms");
+      // Valider les données
+      await validationSchema.validate(
+        { pseudo, email, password, controlPassword, terms: terms.checked },
+        { abortEarly: false }
+      );
 
-    if (password !== controlPassword || !terms.checked) {
-      if (password !== controlPassword)
-        passwordConfirmError.innerHTML =
-          "Les mots de passe ne correspondent pas";
-      if (!terms.checked)
-        termsError.innerHTML = "Veuillez valider les conditions générales";
-    } else {
+      // Si la validation est réussie, envoie les données
       await axios({
         method: "post",
         url: `${process.env.REACT_APP_API_URL}api/user/register`,
         withCredentials: true,
-        data: {
-          pseudo,
-          email,
-          password,
-        },
-      })
-        .then((res) => {
-          console.log(res);
-          setFormSubmit(true);
-        })
-        .catch((err) => {
-          console.log(err);
-          pseudoError.innerHTML = err.response.data.errors.pseudo;
-          emailError.innerHTML = err.response.data.errors.email;
-          if (err.response.data.errors.password)
-            passwordError.innerHTML = err.response.data.errors.password;
-        });
+        data: { pseudo, email, password },
+      });
+      setFormSubmit(true);
+    } catch (err) {
+      const errors = {};
+
+      // Parcourir chaque erreur et stocker uniquement la première pour chaque champ
+      err.inner.forEach((error) => {
+        // Si une erreur pour ce champ n'a pas déjà été enregistrée, l'enregistrer
+        if (!errors[error.path]) {
+          errors[error.path] = error.message;
+        }
+      });
+
+      // Mettre à jour l'UI avec les erreurs
+      for (const [path, message] of Object.entries(errors)) {
+        const errorField = document.querySelector(`.${path}.error`);
+        if (errorField) {
+          errorField.innerHTML = message;
+        }
+      }
     }
   };
+
+  // const handleRegister = async (e) => {
+  //   e.preventDefault();
+  //   const terms = document.getElementById("terms");
+  //   const pseudoError = document.querySelector(".pseudo.error");
+  //   const emailError = document.querySelector(".email.error");
+  //   const passwordError = document.querySelector(".password.error");
+  //   const passwordConfirmError = document.querySelector(
+  //     ".password-confirm.error"
+  //   );
+  //   const termsError = document.querySelector(".terms.error");
+
+  //   passwordConfirmError.innerHTML = "";
+  //   termsError.innerHTML = "";
+
+  //   if (password !== controlPassword || !terms.checked) {
+  //     if (password !== controlPassword)
+  //       passwordConfirmError.innerHTML =
+  //         "Les mots de passe ne correspondent pas";
+  //     if (!terms.checked)
+  //       termsError.innerHTML = "Veuillez valider les conditions générales";
+  //   } else {
+  //     await axios({
+  //       method: "post",
+  //       url: `${process.env.REACT_APP_API_URL}api/user/register`,
+  //       withCredentials: true,
+  //       data: {
+  //         pseudo,
+  //         email,
+  //         password,
+  //       },
+  //     })
+  //       .then((res) => {
+  //         console.log(res);
+  //         setFormSubmit(true);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         pseudoError.innerHTML = err.response.data.errors.pseudo;
+  //         emailError.innerHTML = err.response.data.errors.email;
+  //         if (err.response.data.errors.password)
+  //           passwordError.innerHTML = err.response.data.errors.password;
+  //       });
+  //   }
+  // };
 
   return (
     <>
@@ -108,7 +171,7 @@ const SignUpForm = () => {
             onChange={(e) => setControlPassword(e.target.value)}
             value={controlPassword}
           />
-          <div className="password-confirm error"></div>
+          <div className="controlPassword error"></div>
           <br />
           <input type="checkbox" id="terms" />
           <label htmlFor="terms">
