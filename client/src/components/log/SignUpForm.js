591fd619627844c7fa80e0bae5ddd0c999d85d99
Giogio-armani <giovan.hamadaine@hotmail.com>
2023-11-28 21:11:07 +0100

    correction formulaire inscription

--- client/src/components/log/SignUpForm.js
+++ client/src/components/log/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 import SignInForm from "./SignInForm";
 import * as Yup from "yup";
@@ -31,7 +31,9 @@ const SignUpForm = () => {
   const [password, setPassword] = useState("");
   const [controlPassword, setControlPassword] = useState("");
   const [formSubmit, setFormSubmit] = useState(false);
-
+  const pseudoError = useRef(null);
+  const emailError = useRef(null);
+  const passwordError = useRef(null);
   const handleRegister = async (e) => {
     e.preventDefault();
 
@@ -41,41 +43,59 @@ const SignUpForm = () => {
       element.innerHTML = "";
     });
 
-    try {
-      const terms = document.getElementById("terms");
-      // Valider les données
-      await validationSchema.validate(
+    const terms = document.getElementById("terms");
+    // Valider les données
+    await validationSchema
+      .validate(
         { pseudo, email, password, controlPassword, terms: terms.checked },
         { abortEarly: false }
-      );
+      )
+      .then(async () => {
+        await axios({
+          method: "post",
+          url: `${process.env.REACT_APP_API_URL}api/user/register`,
+          withCredentials: true,
+          data: { pseudo, email, password },
+        })
+          .then((res) => {
+            if (res.data.errors) {
+              console.log(res.data.errors);
+              pseudoError.current.innerHTML = res.data.errors.pseudo;
+              emailError.current.innerHTML = res.data.errors.email;
+            } else {
+              setFormSubmit(true);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            if (err.response.data.errors) {
+              pseudoError.current.innerHTML = err.response.data.errors.pseudo;
+              emailError.current.innerHTML = err.response.data.errors.email;
+            } else if (err.response.data.error.password) {
+              passwordError.current.innerHTML =
+                err.response.data.errors.password;
+            }
+          });
+      })
+      .catch((err) => {
+        const errors = {};
 
-      // Si la validation est réussie, envoie les données
-      await axios({
-        method: "post",
-        url: `${process.env.REACT_APP_API_URL}api/user/register`,
-        withCredentials: true,
-        data: { pseudo, email, password },
-      });
-      setFormSubmit(true);
-    } catch (err) {
-      const errors = {};
+        // Parcourir chaque erreur et stocker uniquement la première pour chaque champ
+        err.inner.forEach((error) => {
+          // Si une erreur pour ce champ n'a pas déjà été enregistrée, l'enregistrer
+          if (!errors[error.path]) {
+            errors[error.path] = error.message;
+          }
+        });
 
-      // Parcourir chaque erreur et stocker uniquement la première pour chaque champ
-      err.inner.forEach((error) => {
-        // Si une erreur pour ce champ n'a pas déjà été enregistrée, l'enregistrer
-        if (!errors[error.path]) {
-          errors[error.path] = error.message;
+        // Mettre à jour l'UI avec les erreurs
+        for (const [path, message] of Object.entries(errors)) {
+          const errorField = document.querySelector(`.${path}.error`);
+          if (errorField) {
+            errorField.innerHTML = message;
+          }
         }
       });
-
-      // Mettre à jour l'UI avec les erreurs
-      for (const [path, message] of Object.entries(errors)) {
-        const errorField = document.querySelector(`.${path}.error`);
-        if (errorField) {
-          errorField.innerHTML = message;
-        }
-      }
-    }
   };
 
   return (
@@ -99,7 +119,7 @@ const SignUpForm = () => {
             onChange={(e) => setPseudo(e.target.value)}
             value={pseudo}
           />
-          <div className="pseudo error"></div>
+          <div className="pseudo error" ref={pseudoError}></div>
           <br />
           <label htmlFor="email">Email</label>
           <br />
@@ -110,7 +130,7 @@ const SignUpForm = () => {
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
-          <div className="email error"></div>
+          <div className="email error" ref={emailError}></div>
           <br />
           <label htmlFor="password">Mot de passe</label>
           <br />
@@ -121,7 +141,7 @@ const SignUpForm = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
-          <div className="password error"></div>
+          <div className="password error" ref={passwordError}></div>
           <br />
           <label htmlFor="password-conf">Confirmer mot de passe</label>
           <br />
